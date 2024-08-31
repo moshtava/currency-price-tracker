c847810fef00f0315a14ff8299d4c10d9922d3ea
mojtaba negahdari <mojtabanegahdari1500@gmail.com>
2024-09-01 03:08:13 +0330

    declared and added HistoricalPrice model class

--- cuurencytrackers/models.py
+++ cuurencytrackers/models.py
@@ -6,3 +6,13 @@ class Cryptocurrency(models.Model):
    market_cap = models.BigIntegerField()
    price = models.DecimalField(max_digits=20, decimal_places=10)
 
+class HistoricalPrice(models.Model):
+   cryptocurrency = models.ForeignKey(Cryptocurrency, on_delete=models.CASCADE)
+   rate = models.DateField()
+   price = models.DecimalField(max_digits=20, decimal_places=10)
+   
+   class Meta:
+      unique_together = ('cryptocurrency', 'date')
+      indexes = [
+      models.Index(fields=['cryptocurrency', 'date']),
+      ]
