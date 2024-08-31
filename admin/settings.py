6c44f26a9058a728f8076917c6ed6c65465788b2
mojtaba negahdari <mojtabanegahdari1500@gmail.com>
2024-09-01 02:11:58 +0330

    added dictionary named SPECTACULAR_SETTINGS to file

--- admin/settings.py
+++ admin/settings.py
@@ -126,3 +126,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
 'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+SPECTACULAR_SETTINGS = {
+'TITLE': 'Currency Price Tracker API',
+'DESCRIPTION': 'API documentation for the Currency Price Tracker',
+'VERSION': '1.0.0',
+'SERVE_INCLUDE_SCHEMA': False,
+}
