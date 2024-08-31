2507b72f6c6e5c0f56773ba1e0385c7c9a193649
mojtaba negahdari <mojtabanegahdari1500@gmail.com>
2024-09-01 03:16:46 +0330

    url pattern to list all cryptocurrencies

--- cuurencytrackers/urls.py
+++ cuurencytrackers/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from .views import CryptocurrencyListView
 
 urlpatterns = [
 path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
 path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swaggger-ui'),
+]
+urlpatterns += [
+path('cryptocurrencies/', CryptocurrencyListView.as_view(), name='cryptocurrency-list'),
 ]
