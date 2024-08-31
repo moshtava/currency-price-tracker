c18e490950dca52fb1eaf72a63ae380ef3d13d4f
mojtaba negahdari <mojtabanegahdari1500@gmail.com>
2024-09-01 02:48:07 +0330

    model class for Cryptocurrency added

--- cuurencytrackers/models.py
+++ cuurencytrackers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Cryptocurrency(models.Model):
+   name = models.CharField(max_length=100)
+   symbol = models.CharField(max_length=10)
+   market_cap = models.BigIntegerField()
+   price = models.DecimalField(max_digits=20, decimal_places=10)
+
