f6d4d0d8de893dd3a0386009a47d6027d18f1ce0
mojtaba negahdari <mojtabanegahdari1500@gmail.com>
2024-09-01 03:39:30 +0330

    added view for 2, i.e, Retrieve details of a specific cryptocurrency by getting it's name.

--- cuurencytrackers/views.py
+++ cuurencytrackers/views.py
@@ -5,3 +5,15 @@ from .serializers import CryptocurrencySerializer
 class CryptocurrencyListView(generics.ListAPIView):
    queryset = Cryptocurrency.objects.all()
    serializer_class = CryptocurrencySerializer
+
+class CryptocurrencyDetailView(generics.RetrieveAPIView):
+   queryset = Cryptocurrency.objects.all()
+   serializer_class = CryptocurrencySerializer
+
+class CryptocurrencyDetailByNameView(generics.RetrieveAPIView):
+   serializer_class = CryptocurrencySerializer
+   lookup_field = 'name'
+
+   def get_queryset(self):
+      name = self.kwargs['name']
+      return Cryptocurrency.objects.filter(name=name)   
