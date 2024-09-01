c02c5cd4e93cb5e8437f12538fce87994ecadec7
mojtaba negahdari <mojtabanegahdari1500@gmail.com>
2024-09-01 17:15:59 +0330

    created RSIView to calculate and return RSI for a cryptocurrency

--- cuurencytrackers/views.py
+++ cuurencytrackers/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from .models import Cryptocurrency, HistoricalPrice
-from .serializers import CryptocurrencySerializer, CryptocurrencyHistoricalPriceSerializer, DailyReturnSerializer
+from .serializers import CryptocurrencySerializer, CryptocurrencyHistoricalPriceSerializer, DailyReturnSerializer, RSISerializer
 from rest_framework.response import Response
+import numpy as np
 
 class CryptocurrencyListView(generics.ListAPIView):
    queryset = Cryptocurrency.objects.all()
@@ -63,3 +64,48 @@ class DailyReturnView(generics.ListAPIView):
          })
       serializer = self.get_serializer(data, many=True)
       return Response(serializer.data)
+
+class RSIView(generics.ListAPIView):
+   serializer_class = RSISerializer
+   
+   def get_queryset(self):
+      name = self.request.query_params.get('name')
+      from_date = self.request.query_params.get('from_date')
+      to_date = self.request.query_params.get('to_date')
+      return HistoricalPrice.objects.filter(
+      cryptocurrency__name=name,
+      date__range=[from_date, to_date]
+      ).order_by('date')
+   
+   def list(self, request, *args, **kwargs):
+      period = int(request.query_params.get('period', 14))
+      queryset = self.get_queryset()
+      prices = np.array([item.price for item in queryset])
+      deltas = np.diff(prices)
+      seed = deltas[:period+1]
+      up = seed[seed >= 0].sum() / period
+      down = -seed[seed < 0].sum() / period
+      rs = up / down
+      rsi = np.zeros_like(prices)
+      rsi[:period] = 100. - 100. / (1. + rs)
+      
+      for i in range(period, len(prices)):
+         delta = deltas[i - 1] 
+      
+      if delta > 0:
+         upval = delta
+         downval = 0.
+      else:
+         upval = 0.
+         downval = -delta
+      
+      up = (up * (period - 1) + upval) / period
+      down = (down * (period - 1) + downval) / period
+      
+      rs = up / down
+      rsi[i] = 100. - 100. / (1. + rs)
+      
+      data = [{'date': queryset[i].date, 'rsi': rsi[i]} for i in range(len(rsi))]
+      serializer = self.get_serializer(data, many=True)
+      return Response(serializer.data)
+   
