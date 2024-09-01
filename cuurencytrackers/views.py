2b857fde5b8398560bbe90d2e305929e88da2807
mojtaba negahdari <mojtabanegahdari1500@gmail.com>
2024-09-01 15:52:05 +0330

    created DailyReturnView to calculate and return daily returns for a cryptocurrency

--- cuurencytrackers/views.py
+++ cuurencytrackers/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Cryptocurrency, HistoricalPrice
-from .serializers import CryptocurrencySerializer, CryptocurrencyHistoricalPriceSerializer
+from .serializers import CryptocurrencySerializer, CryptocurrencyHistoricalPriceSerializer, DailyReturnSerializer
+from rest_framework.response import Response
 
 class CryptocurrencyListView(generics.ListAPIView):
    queryset = Cryptocurrency.objects.all()
@@ -36,3 +37,29 @@ class HistoricalPriceRangeView(generics.ListAPIView):
       cryptocurrency__name=name,
       date__range=[from_date, to_date]
       )
+
+class DailyReturnView(generics.ListAPIView):
+   serializer_class = DailyReturnSerializer
+   
+   def get_queryset(self):
+      name = self.request.query_params.get('name')
+      from_date = self.request.query_params.get('from_date')
+      to_date = self.request.query_params.get('to_date')
+      return HistoricalPrice.objects.filter(
+      cryptocurrency__name=name,
+      date__range=[from_date, to_date]
+      ).order_by('date')
+   
+   def list(self, request, *args, **kwargs):
+      queryset = self.get_queryset()
+      data = []
+      for i in range(1, len(queryset)):
+         today = queryset[i]
+         yesterday = queryset[i-1]
+         daily_return = (today.price - yesterday.price) / yesterday.price
+         data.append({
+         'date': today.date,
+         'daily_return': daily_return
+         })
+      serializer = self.get_serializer(data, many=True)
+      return Response(serializer.data)
