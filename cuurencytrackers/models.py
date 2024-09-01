4fa15aaa14550276a92daad04b742f4feb4d98ce
mojtaba negahdari <mojtabanegahdari1500@gmail.com>
2024-09-01 03:33:46 +0330

    added meta about uniqueness and index of table for cryptocurrency

--- cuurencytrackers/models.py
+++ cuurencytrackers/models.py
@@ -5,6 +5,10 @@ class Cryptocurrency(models.Model):
    symbol = models.CharField(max_length=10)
    market_cap = models.BigIntegerField()
    price = models.DecimalField(max_digits=20, decimal_places=10)
+   
+   class Meta:
+      unique = ('name')
+      indexes = [models.Index(fields=['name']),]
 
 class HistoricalPrice(models.Model):
    cryptocurrency = models.ForeignKey(Cryptocurrency, on_delete=models.CASCADE)
