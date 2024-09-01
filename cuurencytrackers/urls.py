406a9430bf2c940fe19dd73adc43e19f491e23ae
mojtaba negahdari <mojtabanegahdari1500@gmail.com>
2024-09-01 04:32:35 +0330

    added url pattern for 3, i.e, Get historical prices for a specific cryptocurrency.

--- cuurencytrackers/urls.py
+++ cuurencytrackers/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from .views import CryptocurrencyListView, CryptocurrencyDetailByNameView
+from .views import CryptocurrencyListView, CryptocurrencyDetailByNameView, HistoricalPriceListView
 
 urlpatterns = [
 path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
@@ -9,4 +9,5 @@ path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagg
 urlpatterns += [
 path('cryptocurrencies/', CryptocurrencyListView.as_view(), name='cryptocurrency-list'),
 path('cryptocurrencies/name/<str:name>/', CryptocurrencyDetailByNameView.as_view(), name='cryptocurrency-detail-by-name'),
+path('cryptocurrencies/<str:name>/historical-prices/', HistoricalPriceListView.as_view(), name='historical-price-list'),
 ]
