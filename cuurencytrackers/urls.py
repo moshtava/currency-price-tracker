2470794f2264017022c5ad452ca55b2b34a4cb44
mojtaba negahdari <mojtabanegahdari1500@gmail.com>
2024-09-01 15:52:31 +0330

    Added URL pattern for DailyReturnView to handle daily return queries

--- cuurencytrackers/urls.py
+++ cuurencytrackers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from .views import CryptocurrencyListView, CryptocurrencyDetailByNameView, HistoricalPriceListView, HistoricalPriceRangeView
+from .views import DailyReturnView
+from .views import CryptocurrencyListView, CryptocurrencyDetailByNameView, HistoricalPriceListView, HistoricalPriceRangeView, DailyReturnView
 
 urlpatterns = [
 path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
@@ -11,4 +12,5 @@ path('cryptocurrencies/', CryptocurrencyListView.as_view(), name='cryptocurrency
 path('cryptocurrencies/name/<str:name>/', CryptocurrencyDetailByNameView.as_view(), name='cryptocurrency-detail-by-name'),
 path('cryptocurrencies/<str:name>/historical-prices/', HistoricalPriceListView.as_view(), name='historical-price-list'),
 path('historical-prices/', HistoricalPriceRangeView.as_view(), name='historical-price-range'),
+path('daily-returns/', DailyReturnView.as_view(), name='daily-returns'),
 ]
