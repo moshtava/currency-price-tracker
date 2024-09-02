97931fc6ede74703361075cec9610c33dd82c8b4
mojtaba negahdari <mojtabanegahdari1500@gmail.com>
2024-09-02 16:44:40 +0330

    added some more comments to granting script for more clarity.

--- init.sql
+++ init.sql
@@ -1,6 +1,8 @@
 DO $$
 BEGIN
--- Check if the role exists
+-- Check if the role exists. we need it because it'll make the process of creating role
+-- smoother and more practical. otherwise, it would return an error if the role already
+-- exists, which is typically not what we want. instead, we want it to update if it already exists.
 IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'postgres') THEN
 -- Create the role if it doesn't exist
 CREATE ROLE postgres WITH LOGIN PASSWORD 'password';
